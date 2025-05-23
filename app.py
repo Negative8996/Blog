da5859553c110893a07b00825b2c37a7f3419ece
Negative8996 <noah.gratzius1@gmail.com>
2025-05-23 18:41:41 +0200

    Update app.py 200 to 512

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ socketio = SocketIO(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
